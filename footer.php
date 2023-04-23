afde15ba729bd752a90264ba7d2a7b483b0f35c3
Chethan S Poojary <chethanspoojary1@gmail.com>
2023-04-23 22:58:18 +0530

    Complete Home Design

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -8,20 +9,65 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+<?php get_template_part('sidebar-templates/sidebar', 'footerfull'); ?>
 
 <div class="wrapper" id="wrapper-footer">
 
-	
+    <div class="footer-testimonial">
+        <div class="container">
+            <div class="testimonial">
+                <div class="stars">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <div class="quote">
+                    "Amazing customer service and very good price will be definitely getting another one through these guys when needed so much help thankyou."
+                </div>
+                <div class="author">
+                    Damon, facebook review
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="footer-contact-info">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="title">Want to know more?</div>
+                    <div class="desc">To find out more about our services please contact us using any of the methods below, or alternatively fill in the contact form and a representative will be in touch as soon as possible.</div>
+                    <div class="contact-info">
+                        <div class="phone"><i class="fa fa-phone"></i> Call today 1800 927 831</div>
+                        <div class="email"><i class="fa fa-envelope-o"></i> Email info@westcoastwaste.com.au</div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-form-holder">
+                        <?php echo do_shortcode('[contact-form-7 id="24" title="Footer Contact Form"]'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <div class="container">
+            © Copyright chethanspoojary.com  <span>//</span> <a href="/terms-and-conditions/">Terms &amp; Conditions</a> <span>//</span> <a href="https://chethanspoojary.com"> Website by CHethan S Poojary</a>
+        </div>
+    </div>
 
 </div><!-- #wrapper-footer -->
 
-<?php // Closing div#page from header.php. ?>
+<?php // Closing div#page from header.php. 
+?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
@@ -29,4 +75,3 @@ $container = get_theme_mod( 'understrap_container_type' );
 </body>
 
 </html>
-
